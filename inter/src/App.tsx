891ed6ba8c93ad28f933b0b64a0120ef0e29ec53
Monke72 <wera16307@gmail.com>
2025-04-27 15:11:37 +0400

    first commit

---
+++ inter/src/App.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from "react";
+
+import ThemesAll from "./Component/ThemesAll";
+
+const App: FC = () => {
+  return (
+    <>
+      <ThemesAll />
+    </>
+  );
+};
+
+export default App;
---
+++ inter/src/Component/ThemeEl.tsx
@@ -0,0 +1,18 @@
+import React, { FC } from "react";
+import { useState } from "react";
+import { ITasks } from "../taskArray";
+interface IThemeEl {
+  el: ITasks;
+}
+
+const ThemeEl: FC<IThemeEl> = ({ el }) => {
+  const [show, setShow] = useState<boolean>(false);
+  return (
+    <li>
+      <h3 onClick={() => setShow((prev) => !prev)}>{el.question}</h3>
+      {show && <p>{el.answer}</p>}
+    </li>
+  );
+};
+
+export default ThemeEl;
